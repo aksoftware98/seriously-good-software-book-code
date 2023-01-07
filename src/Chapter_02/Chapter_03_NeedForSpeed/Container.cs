8b652affdfed7d5bfa8106ccdc4313a0a313d4a1
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-01-07 17:55:13 +0400

    Implement and test adding a water at constant speed approach

--- src/Chapter_02/Chapter_03_NeedForSpeed/Container.cs
+++ src/Chapter_02/Chapter_03_NeedForSpeed/Container.cs
@@ -11,4 +11,61 @@ namespace Chapter_03_NeedForSpeed;
 /// </summary>
 internal class ContainerForConstantAddingWater
 {
+
+    private GroupForConstantAddingWater? _group = null;
+    public ContainerForConstantAddingWater()
+    {
+        _group = new(this);
+    }
+
+	public void AddWater(double amount)
+    {
+		var newAmountPerContainer = amount / _group._members.Count;
+		_group._amountPerContainer += newAmountPerContainer;
+	}
+
+	public void ConnectTo(ContainerForConstantAddingWater otherContainer)
+    {
+		if (_group == otherContainer._group)
+			return;
+
+		var currentGroupSize = _group._members.Count;
+		var otherGroupSize = otherContainer._group._members.Count;
+
+		var totalAmount = _group._amountPerContainer * currentGroupSize;
+		var otherGroupTotalAmount = otherContainer._group._amountPerContainer * otherGroupSize;
+		var newTotalAmount = (totalAmount + otherGroupTotalAmount) / (currentGroupSize + otherGroupSize);
+
+		_group._amountPerContainer = newTotalAmount / _group._members.Count;
+		foreach (var item in otherContainer._group._members)
+		{
+			item._group = _group;
+			_group._members.Add(item);
+		}
+    }
+
+	public double GetAmount()
+    {
+        return _group?.GetAmountPerContainer() ?? 0;
+    }
+
+	internal class GroupForConstantAddingWater
+	{
+		internal double _amountPerContainer;
+		internal ICollection<ContainerForConstantAddingWater> _members;
+
+		public GroupForConstantAddingWater(ContainerForConstantAddingWater container)
+		{
+			_members = new HashSet<ContainerForConstantAddingWater>();
+			_members.Add(container);
+		}
+
+		public double GetAmountPerContainer()
+		{
+			return _amountPerContainer;
+		}
+
+	}
 }
+
+
--- src/Chapter_02/Chapter_03_NeedForSpeed/Program.cs
+++ src/Chapter_02/Chapter_03_NeedForSpeed/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Chapter_03_NeedForSpeed;
+
+Console.WriteLine("Speed: Making adding water a constant speed:");
+
+var containerA = new ContainerForConstantAddingWater();
+var containerB = new ContainerForConstantAddingWater();
+var containerC = new ContainerForConstantAddingWater();
+var containerD = new ContainerForConstantAddingWater();
+
+containerA.AddWater(12);
+containerB.AddWater(8);
+containerA.ConnectTo(containerB);
+
+Console.WriteLine($"Container A: {containerA.GetAmount()}");
+Console.WriteLine($"Container B: {containerB.GetAmount()}");
+Console.WriteLine($"Container C: {containerC.GetAmount()}");
+Console.WriteLine($"Container D: {containerD.GetAmount()}");
+
+Console.ReadKey(); 
