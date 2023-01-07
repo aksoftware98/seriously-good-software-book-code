18d8da3cc323e36f9eb19fbeea9f137b169084d7
Ahmad Mozaffar <aksoftware98@hotmail.com>
2023-01-07 15:28:03 +0400

    Create the project of chapter 2

---
+++ src/Chapter_02/Chapter_03_NeedForSpeed/Container.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_03_NeedForSpeed;
+
+/// <summary>
+/// Contianer class optimzed for the process of adding water to be done a single step 
+/// </summary>
+internal class ContainerForConstantAddingWater
+{
+}
